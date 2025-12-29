15d775ac4a0b22c394801785c40fe401f9616602
ANNE S. <7ry9kjsm7t@privaterelay.appleid.com>
2025-12-30 04:39:16 +0700

    script.js

--- script.js
+++ script.js
@@ -1,107 +1,3 @@
-const cards = [
-  "https://i.postimg.cc/Zn6Thm4g/card01-png.png",
-  "https://i.postimg.cc/W1YcCz03/card02-png.png",
-  "https://i.postimg.cc/pVKxr2tP/card03-png.png",
-  "https://i.postimg.cc/v85MTQwr/card04-png.png",
-  "https://i.postimg.cc/yxCHjGcm/card05-png.png",
-  "https://i.postimg.cc/bvM8DrvB/card06-png.png",
-  "https://i.postimg.cc/65NWf8hX/card07-png.png",
-  "https://i.postimg.cc/x8L0SQ9V/card08-png.png",
-  "https://i.postimg.cc/W11P5gqV/card09-png.png",
-  "https://i.postimg.cc/Df1F02KK/card10-png.png",
-  "https://i.postimg.cc/QCgryqqX/card11-png.png",
-  "https://i.postimg.cc/3Rvhgh4j/card12-png.png",
-  "https://i.postimg.cc/9fSVR0Mm/card13-png.png",
-  "https://i.postimg.cc/zDhDhCK9/card14-png.png",
-  "https://i.postimg.cc/0QT83jRd/card15-png.png",
-  "https://i.postimg.cc/rwwLfx03/card16-png.png",
-  "https://i.postimg.cc/ry5cs82b/card17-png.png",
-  "https://i.postimg.cc/L5kMbV3r/card18-png.png",
-  "https://i.postimg.cc/MHJS5LVj/card19-png.png",
-  "https://i.postimg.cc/3x83zzVQ/card20-png.png",
-  "https://i.postimg.cc/9X9X9dZJ/card21-png.png",
-  "https://i.postimg.cc/RFkM8h5T/card22-png.png",
-  "https://i.postimg.cc/zX5NR9Jy/card23-png.png",
-  "https://i.postimg.cc/7PSDhxrB/card24-png.png",
-  "https://i.postimg.cc/nrfJ2WBg/card25-png.png",
-  "https://i.postimg.cc/zBgNnNb1/card26-png.png",
-  "https://i.postimg.cc/R0q4ZKvW/card27-png.png",
-  "https://i.postimg.cc/qq2kdrpB/card28-png.png",
-  "https://i.postimg.cc/y6R6Rmcj/card29-png.png",
-  "https://i.postimg.cc/5N1JFZxJ/card30-png.png",
-  "https://i.postimg.cc/T2nG1RvQ/card31-png.png",
-  "https://i.postimg.cc/FzbQBxVP/card32-png.png",
-  "https://i.postimg.cc/4d9GzG7x/card33-png.png",
-  "https://i.postimg.cc/jdrx4DhV/card34-png.png",
-  "https://i.postimg.cc/ZRGTgntx/card35-png.png",
-  "https://i.postimg.cc/wvLK1kZy/card36-png.png",
-  "https://i.postimg.cc/yYBKSwV4/card37-png.png",
-  "https://i.postimg.cc/3r27NKQF/card38-png.png",
-  "https://i.postimg.cc/cCGShzfq/card39-png.png",
-  "https://i.postimg.cc/d0Xvk31V/card40-png.png",
-  "https://i.postimg.cc/VLcfWJW3/card41-png.png",
-  "https://i.postimg.cc/4ypJTGsP/card42-png.png",
-  "https://i.postimg.cc/7ZtrdhSL/card43-png.png",
-  "https://i.postimg.cc/sf5yxVRs/card44-png.png",
-  "https://i.postimg.cc/T1jX4cc1/card45-png.png",
-  "https://i.postimg.cc/gjbWs78v/card46-png.png",
-  "https://i.postimg.cc/pd7xhrdk/card47-png.png",
-  "https://i.postimg.cc/kXdJv2vh/card48-png.png",
-  "https://i.postimg.cc/3rGrGXmD/card49-png.png",
-  "https://i.postimg.cc/Z55hwpBX/card50-png.png",
-  "https://i.postimg.cc/NGRYFgwq/card51-png.png",
-  "https://i.postimg.cc/Vv9cVFWx/card52-png.png",
-  "https://i.postimg.cc/FRLvjvdk/card53-png.png",
-  "https://i.postimg.cc/FK1NH3hG/card54-png.png",
-  "https://i.postimg.cc/Vststqnp/card55-png.png",
-  "https://i.postimg.cc/Xq9V0Wn4/card56-png.png",
-  "https://i.postimg.cc/Z55hwpBc/card57-png.png",
-  "https://i.postimg.cc/MZ1xHWJL/card58-png.png",
-  "https://i.postimg.cc/wM5dGkkp/card59-png.png",
-  "https://i.postimg.cc/mDH4Q4zH/card60-png.png",
-  "https://i.postimg.cc/kgT7CfPm/card61-png.png",
-  "https://i.postimg.cc/Dzzy4RPz/card62-png.png",
-  "https://i.postimg.cc/3N2KHhYS/card63-png.png",
-  "https://i.postimg.cc/MKxwjhWZ/card64-png.png",
-  "https://i.postimg.cc/pVKxr2Mq/card65-png.png",
-  "https://i.postimg.cc/yxCHjGcr/card66-png.png",
-  "https://i.postimg.cc/V6Cwjwrq/card67-png.png",
-  "https://i.postimg.cc/m2GLyPSB/card68-png.png",
-  "https://i.postimg.cc/MH1W8wqd/card69-png.png",
-  "https://i.postimg.cc/Y0Qpw7MD/card70-png.png",
-  "https://i.postimg.cc/VL1wb3Yc/card71-png.png",
-  "https://i.postimg.cc/fWYZywQK/card72-png.png",
-  "https://i.postimg.cc/90HhbkZk/card73-png.png",
-  "https://i.postimg.cc/50CJwJYW/card74-png.png",
-  "https://i.postimg.cc/Yqwrzvx2/card75-png.png",
-  "https://i.postimg.cc/nzyH5rbk/card76-png.png",
-  "https://i.postimg.cc/RZpxyhwJ/card77-png.png",
-  "https://i.postimg.cc/HsHm7fpG/card78-png.png",
-  "https://i.postimg.cc/NGRYFgv8/card79-png.png",
-  "https://i.postimg.cc/RhxBdYKX/card80-png.png",
-  "https://i.postimg.cc/8C3DJc5D/card81-png.png",
-  "https://i.postimg.cc/1z4s3wyH/card82-png.png",
-  "https://i.postimg.cc/VvXYcwmx/card83-png.png",
-  "https://i.postimg.cc/mgpWJk7Z/card84-png.png",
-  "https://i.postimg.cc/3r27NKHT/card85-png.png",
-  "https://i.postimg.cc/0jGqBCC5/card86-png.png",
-  "https://i.postimg.cc/ZnzSHQp6/card87-png.png",
-  "https://i.postimg.cc/pd7xhrTH/card88-png.png",
-  "https://i.postimg.cc/HWMWMwbD/card89-png.png",
-  "https://i.postimg.cc/05S5SYDp/card90-png.png",
-  "https://i.postimg.cc/8zzVZMfg/card91-png.png",
-  "https://i.postimg.cc/jqyK2srV/card92-png.png",
-  "https://i.postimg.cc/Hn0Chttd/card93-png.png",
-  "https://i.postimg.cc/856GhGJC/card94-png.png",
-  "https://i.postimg.cc/XvX4Ydny/card95-png.png",
-  "https://i.postimg.cc/XN5N5fdH/card96-png.png",
-  "https://i.postimg.cc/XN5N5fdf/card97-png.png",
-  "https://i.postimg.cc/65wKGD9y/card98-png.png",
-  "https://i.postimg.cc/CMbY5Fpr/card99-png.png",
-  "https://i.postimg.cc/3NjHcZFq/card100-png.png"
-                    
-];
-
 const questions = [
   "What part of yourself do you think people misunderstand the most?",
   "When you feel overwhelmed, what helps you feel grounded again?",
@@ -209,28 +105,142 @@ const questions = [
 document.addEventListener("DOMContentLoaded", () => {
   const intro = document.getElementById("intro");
   const enterBtn = document.getElementById("enterBtn");
+
   const menu = document.getElementById("menu");
   const menuCard = document.getElementById("menuCard");
-  const mainContent = document.getElementById("mainContent");
-  const backToMenu = document.getElementById("backToMenu");
+  const menuQuestion = document.getElementById("menuQuestion");
+
+  const dailyCard = document.getElementById("dailyCard");
+  const backToMenu1 = document.getElementById("backToMenu1");
+
+  const whisperBox = document.getElementById("whisperBox");
+  const backToMenu2 = document.getElementById("backToMenu2");
 
   const cardImage = document.getElementById("cardImage");
-  const note = document.getElementById("note");  
+  const note = document.getElementById("note");
+  const hint = document.getElementById("hint");
   const drawBtn = document.getElementById("drawBtn");
   const resetBtn = document.getElementById("resetBtn");
-  const hint = document.getElementById("hint");
-  const whisper = document.querySelector(".whisper");
-  const footer = document.querySelector(".footer");
 
-  function setBackground(isMetaphor) {
-    if (isMetaphor) {
-      document.body.classList.add("night");
-    } else {
-      document.body.classList.remove("night");
-    }
+  const revealBtn = document.getElementById("revealBtn");
+  const questionText = document.getElementById("questionText");
+  const formWrapper = document.getElementById("formWrapper");
+
+  const cards = [
+  "https://i.postimg.cc/Zn6Thm4g/card01-png.png",
+  "https://i.postimg.cc/W1YcCz03/card02-png.png",
+  "https://i.postimg.cc/pVKxr2tP/card03-png.png",
+  "https://i.postimg.cc/v85MTQwr/card04-png.png",
+  "https://i.postimg.cc/yxCHjGcm/card05-png.png",
+  "https://i.postimg.cc/bvM8DrvB/card06-png.png",
+  "https://i.postimg.cc/65NWf8hX/card07-png.png",
+  "https://i.postimg.cc/x8L0SQ9V/card08-png.png",
+  "https://i.postimg.cc/W11P5gqV/card09-png.png",
+  "https://i.postimg.cc/Df1F02KK/card10-png.png",
+  "https://i.postimg.cc/QCgryqqX/card11-png.png",
+  "https://i.postimg.cc/3Rvhgh4j/card12-png.png",
+  "https://i.postimg.cc/9fSVR0Mm/card13-png.png",
+  "https://i.postimg.cc/zDhDhCK9/card14-png.png",
+  "https://i.postimg.cc/0QT83jRd/card15-png.png",
+  "https://i.postimg.cc/rwwLfx03/card16-png.png",
+  "https://i.postimg.cc/ry5cs82b/card17-png.png",
+  "https://i.postimg.cc/L5kMbV3r/card18-png.png",
+  "https://i.postimg.cc/MHJS5LVj/card19-png.png",
+  "https://i.postimg.cc/3x83zzVQ/card20-png.png",
+  "https://i.postimg.cc/9X9X9dZJ/card21-png.png",
+  "https://i.postimg.cc/RFkM8h5T/card22-png.png",
+  "https://i.postimg.cc/zX5NR9Jy/card23-png.png",
+  "https://i.postimg.cc/7PSDhxrB/card24-png.png",
+  "https://i.postimg.cc/nrfJ2WBg/card25-png.png",
+  "https://i.postimg.cc/zBgNnNb1/card26-png.png",
+  "https://i.postimg.cc/R0q4ZKvW/card27-png.png",
+  "https://i.postimg.cc/qq2kdrpB/card28-png.png",
+  "https://i.postimg.cc/y6R6Rmcj/card29-png.png",
+  "https://i.postimg.cc/5N1JFZxJ/card30-png.png",
+  "https://i.postimg.cc/T2nG1RvQ/card31-png.png",
+  "https://i.postimg.cc/FzbQBxVP/card32-png.png",
+  "https://i.postimg.cc/4d9GzG7x/card33-png.png",
+  "https://i.postimg.cc/jdrx4DhV/card34-png.png",
+  "https://i.postimg.cc/ZRGTgntx/card35-png.png",
+  "https://i.postimg.cc/wvLK1kZy/card36-png.png",
+  "https://i.postimg.cc/yYBKSwV4/card37-png.png",
+  "https://i.postimg.cc/3r27NKQF/card38-png.png",
+  "https://i.postimg.cc/cCGShzfq/card39-png.png",
+  "https://i.postimg.cc/d0Xvk31V/card40-png.png",
+  "https://i.postimg.cc/VLcfWJW3/card41-png.png",
+  "https://i.postimg.cc/4ypJTGsP/card42-png.png",
+  "https://i.postimg.cc/7ZtrdhSL/card43-png.png",
+  "https://i.postimg.cc/sf5yxVRs/card44-png.png",
+  "https://i.postimg.cc/T1jX4cc1/card45-png.png",
+  "https://i.postimg.cc/gjbWs78v/card46-png.png",
+  "https://i.postimg.cc/pd7xhrdk/card47-png.png",
+  "https://i.postimg.cc/kXdJv2vh/card48-png.png",
+  "https://i.postimg.cc/3rGrGXmD/card49-png.png",
+  "https://i.postimg.cc/Z55hwpBX/card50-png.png",
+  "https://i.postimg.cc/NGRYFgwq/card51-png.png",
+  "https://i.postimg.cc/Vv9cVFWx/card52-png.png",
+  "https://i.postimg.cc/FRLvjvdk/card53-png.png",
+  "https://i.postimg.cc/FK1NH3hG/card54-png.png",
+  "https://i.postimg.cc/Vststqnp/card55-png.png",
+  "https://i.postimg.cc/Xq9V0Wn4/card56-png.png",
+  "https://i.postimg.cc/Z55hwpBc/card57-png.png",
+  "https://i.postimg.cc/MZ1xHWJL/card58-png.png",
+  "https://i.postimg.cc/wM5dGkkp/card59-png.png",
+  "https://i.postimg.cc/mDH4Q4zH/card60-png.png",
+  "https://i.postimg.cc/kgT7CfPm/card61-png.png",
+  "https://i.postimg.cc/Dzzy4RPz/card62-png.png",
+  "https://i.postimg.cc/3N2KHhYS/card63-png.png",
+  "https://i.postimg.cc/MKxwjhWZ/card64-png.png",
+  "https://i.postimg.cc/pVKxr2Mq/card65-png.png",
+  "https://i.postimg.cc/yxCHjGcr/card66-png.png",
+  "https://i.postimg.cc/V6Cwjwrq/card67-png.png",
+  "https://i.postimg.cc/m2GLyPSB/card68-png.png",
+  "https://i.postimg.cc/MH1W8wqd/card69-png.png",
+  "https://i.postimg.cc/Y0Qpw7MD/card70-png.png",
+  "https://i.postimg.cc/VL1wb3Yc/card71-png.png",
+  "https://i.postimg.cc/fWYZywQK/card72-png.png",
+  "https://i.postimg.cc/90HhbkZk/card73-png.png",
+  "https://i.postimg.cc/50CJwJYW/card74-png.png",
+  "https://i.postimg.cc/Yqwrzvx2/card75-png.png",
+  "https://i.postimg.cc/nzyH5rbk/card76-png.png",
+  "https://i.postimg.cc/RZpxyhwJ/card77-png.png",
+  "https://i.postimg.cc/HsHm7fpG/card78-png.png",
+  "https://i.postimg.cc/NGRYFgv8/card79-png.png",
+  "https://i.postimg.cc/RhxBdYKX/card80-png.png",
+  "https://i.postimg.cc/8C3DJc5D/card81-png.png",
+  "https://i.postimg.cc/1z4s3wyH/card82-png.png",
+  "https://i.postimg.cc/VvXYcwmx/card83-png.png",
+  "https://i.postimg.cc/mgpWJk7Z/card84-png.png",
+  "https://i.postimg.cc/3r27NKHT/card85-png.png",
+  "https://i.postimg.cc/0jGqBCC5/card86-png.png",
+  "https://i.postimg.cc/ZnzSHQp6/card87-png.png",
+  "https://i.postimg.cc/pd7xhrTH/card88-png.png",
+  "https://i.postimg.cc/HWMWMwbD/card89-png.png",
+  "https://i.postimg.cc/05S5SYDp/card90-png.png",
+  "https://i.postimg.cc/8zzVZMfg/card91-png.png",
+  "https://i.postimg.cc/jqyK2srV/card92-png.png",
+  "https://i.postimg.cc/Hn0Chttd/card93-png.png",
+  "https://i.postimg.cc/856GhGJC/card94-png.png",
+  "https://i.postimg.cc/XvX4Ydny/card95-png.png",
+  "https://i.postimg.cc/XN5N5fdH/card96-png.png",
+  "https://i.postimg.cc/XN5N5fdf/card97-png.png",
+  "https://i.postimg.cc/65wKGD9y/card98-png.png",
+  "https://i.postimg.cc/CMbY5Fpr/card99-png.png",
+  "https://i.postimg.cc/3NjHcZFq/card100-png.png"
+                    
+];
+
+
+  const todayKey = new Date().toDateString();
+  const savedIndex = localStorage.getItem(todayKey);
+
+  function showMenu() {
+    dailyCard.style.display = "none";
+    whisperBox.style.display = "none";
+    menu.style.display = "flex";
   }
 
-  function resetUI() {
+  function resetDailyCard() {
     cardImage.style.display = "none";
     note.style.display = "none";
     note.textContent = "";
@@ -238,29 +248,22 @@ document.addEventListener("DOMContentLoaded", () => {
     hint.textContent = "";
     drawBtn.style.display = "inline-block";
     resetBtn.style.display = "none";
-    setBackground(false);
+    document.body.classList.remove("night");
   }
 
-  const todayKey = new Date().toDateString();
-  const savedIndex = localStorage.getItem(todayKey);
-
-  menu.style.display = "none";
-  mainContent.style.display = "none";
-
   enterBtn.addEventListener("click", () => {
-    intro.style.opacity = "0";
+    intro.style.opacity = 0;
     intro.style.pointerEvents = "none";
-
     setTimeout(() => {
       intro.style.display = "none";
-      menu.style.display = "flex";
+      showMenu();
     }, 600);
   });
 
   menuCard.addEventListener("click", () => {
     menu.style.display = "none";
-    mainContent.style.display = "flex";
-    resetUI();
+    dailyCard.style.display = "flex";
+    resetDailyCard();
 
     if (savedIndex !== null) {
       const index = parseInt(savedIndex, 10);
@@ -273,15 +276,19 @@ document.addEventListener("DOMContentLoaded", () => {
       hint.style.display = "block";
       drawBtn.style.display = "none";
       resetBtn.style.display = "inline-block";
-      setBackground(index >= 75);
+      if (index >= 75) document.body.classList.add("night");
     }
   });
 
-  backToMenu.addEventListener("click", () => {
-    mainContent.style.display = "none";
-    menu.style.display = "flex";
-    setBackground(false);
+  menuQuestion.addEventListener("click", () => {
+    menu.style.display = "none";
+    whisperBox.style.display = "flex";
+    questionText.textContent = "Tap when you are ready.";
+    formWrapper.classList.add("hidden");
   });
+
+  backToMenu1.addEventListener("click", showMenu);
+  backToMenu2.addEventListener("click", showMenu);
 
   drawBtn.addEventListener("click", () => {
     const randomIndex = Math.floor(Math.random() * cards.length);
@@ -295,14 +302,20 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem(todayKey, randomIndex);
     drawBtn.style.display = "none";
     resetBtn.style.display = "inline-block";
-    setBackground(randomIndex >= 75);
+    if (randomIndex >= 75) document.body.classList.add("night");
   });
 
   resetBtn.addEventListener("click", () => {
     localStorage.removeItem(todayKey);
-    resetUI();
-    note.classList.remove("is-active");
+    resetDailyCard();
+    showMenu();
   });
 
-  resetUI();
+  revealBtn.addEventListener("click", () => {
+    if (typeof questions !== "undefined" && questions.length > 0) {
+      const idx = Math.floor(Math.random() * questions.length);
+      questionText.textContent = questions[idx];
+      formWrapper.classList.remove("hidden");
+    }
+  });
 });
